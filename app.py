f5388fbbf6dc1f8ea7c83455a88f238a589dd96e
iamnakarin <32155938+iamnakarin@users.noreply.github.com>
2022-12-07 16:01:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-
 import streamlit as st
-
-st.write("Hello")
+import numpy as np
+import pandas as pd
+st.title('Basic Streamlit by Uncle Engineer')
+st.write("ลุงจะมาสอน Python Visualization ด้วย Streamlit เร็วๆนี้")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+st.line_chart(chart_data)
