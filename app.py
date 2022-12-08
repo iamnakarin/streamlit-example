61b990181728f6c4268eb98b840689c0cac75f29
iamnakarin <32155938+iamnakarin@users.noreply.github.com>
2022-12-08 12:05:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ st.write(model.predict([[txt1,txt2,txt3]]))
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
-  st.write(dataframe)
+  st.write(df)
