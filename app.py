eca402bef8908d9398131625304960b600b11900
iamnakarin <32155938+iamnakarin@users.noreply.github.com>
2022-12-08 11:06:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 
 from sklearn import tree
 import streamlit as st
+import pandas as pd
 model = tree.DecisionTreeClassifier()
 feature =pd.read_csv('https://drive.google.com/file/d/1w5vSYGK2-hpZRyhnZGZmSJN3Kt5hylfD/view?usp=share_link')
 label =pd.read_csv('https://drive.google.com/file/d/1-a_JQXsNJoyeYoo30P4YKNAljDWmPRn8/view?usp=share_link')
