b98c5935ff44a81119b701c9eb728c48e8157902
iamnakarin <32155938+iamnakarin@users.noreply.github.com>
2022-12-08 11:51:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from sklearn import tree
 import streamlit as st
 import pandas as pd
 model = tree.DecisionTreeClassifier()
-feature =pd.read_csv('https://apps.samsenwit.ac.th/vichakarn/file/data.csv')
-label =pd.read_csv('https://apps.samsenwit.ac.th/vichakarn/file/label.csv')
+url1=st.file_uploader("https://apps.samsenwit.ac.th/vichakarn/file/data.csv", type={"csv", "txt"})
+feature=pd.read_csv(url1)
+url2=st.file_uploader("ttps://apps.samsenwit.ac.th/vichakarn/file/label.csvv", type={"csv", "txt"})
+label =pd.read_csv(url2)
 #txt1=st.number_input('Enter a number1')
 #txt2=st.number_input('Enter a number2')
 #txt3=st.number_input('Enter a number3')
